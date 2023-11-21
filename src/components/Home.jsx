b789857ac26e6e09a3077c68f25bb03fab3bcbce
Cristianp24 <113152286+Cristianp24@users.noreply.github.com>
2023-11-20 21:22:10 -0300

    Fix Img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { styles } from "../styles";
-import perfil from "../assets/perfil.jpg";
+import perfil from "../assets/perfil.png";
 const Hero = () => {
 
   const openCV = () => {
@@ -11,9 +11,13 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
+        
         <div className='flex flex-col justify-center items-center mt-5'>
+          
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          
         </div>
 
         <div>
@@ -21,12 +25,10 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Cristian</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Developer Web FullStack      <br className='sm:block hidden' />           
-            <button className='mt-5 ml-5 mb-5 bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary hover:scale-105' onClick={openCV}>View CV</button>
-            
+            Developer Web FullStack      <br className='sm:block hidden' />      <button className='button-cv' onClick={openCV}>View CV</button>       
+           
+            <img src={perfil} className='Img-Home' />
           </p>
-          <img src={perfil} className='Img-Home' />
-          
         </div>
       </div>
 
